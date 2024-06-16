d68a044609c2bdee0ab84d30efe5dd77286af53b
Shadman Rahman <shadmxn@gmail.com>
2024-06-16 18:48:18 -0400

    added snake initials

--- client.js
+++ client.js
@@ -1,18 +1,23 @@
 const net = require("net");
 
-// Establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
     host: 'localhost',
     port: 50541
   });
 
-  // Interpret incoming data as text
   conn.setEncoding("utf8");
 
-  // Handle incoming data and log it to the console
   conn.on("data", (data) => {
     console.log("Server says: ", data);
+  });
+
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+
+
+    conn.write("Name: SNK");
   });
 
   return conn;
