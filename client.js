f09c25b80e5a5b09fba1bd19dc90273aa293bd59
Shadman Rahman <shadmxn@gmail.com>
2024-06-16 19:30:53 -0400

    WASD Movement

--- client.js
+++ client.js
@@ -16,7 +16,7 @@ const connect = function() {
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
 
-    conn.write("Name: SNK");
+    conn.write("Name: Shadman");
 
     // conn.write("Move: up");
   });
