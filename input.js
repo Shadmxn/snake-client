69beb0c3164c131e7a74fbce9369c1f9b23d9e70
Shadman Rahman <shadmxn@gmail.com>
2024-06-16 19:57:13 -0400

    added 5 messages

--- input.js
+++ input.js
@@ -28,6 +28,24 @@ const handleUserInput = (key) => {
     console.log("Move: right");
     connection.write("Move: right");
   }
+
+  if (key === '1') {
+    console.log("Say: Welcome to Snake!");
+    connection.write("Say: Welcome to Snake!");
+  } else if (key === '2') {
+    console.log("Say: Hope you enjoy playing!");
+    connection.write("Say: Hope you enjoy playing!");
+  } else if (key === '3') {
+    console.log("Say: Best of luck!");
+    connection.write("Say: Best of luck!");
+  } else if (key === '4') {
+    console.log("Say: Great Job!");
+    connection.write("Say: Great Job!");
+  } else if (key === '5') {
+    console.log("Say: See you next time!");
+    connection.write("Say: See you next time!");
+  }
 };
+
 
 module.exports = { setupInput };
