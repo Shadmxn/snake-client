e3b4982cd2a7cd6d842c4333800c3e9a6f36937a
Shadman Rahman <shadmxn@gmail.com>
2024-06-16 19:31:07 -0400

    WASD Movement

--- play.js
+++ play.js
@@ -2,6 +2,9 @@ const { connect } = require("./client");
 const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-connect();
 
-setupInput();
+// Connect to the server and store the connection object
+const connection = connect();
+
+// Pass that connection object to `setupInput`
+setupInput(connection);
